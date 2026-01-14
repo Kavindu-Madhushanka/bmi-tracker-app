8b2aa6ba8e3a907e89c99990021d7c48979c8166
Kavindu-Madhushanka <madu10275@gmail.com>
2026-01-14 12:12:29 +0530

    modify user model

--- bmibackend/bmibackend/Models/User.cs
+++ bmibackend/bmibackend/Models/User.cs
@@ -1,10 +1,30 @@
-﻿namespace bmibackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace bmibackend.Models
 {
+    [Table("users")]
     public class User
     {
-        public int Id { get; set; } 
-        public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("User_ID")]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Column("Full_Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
