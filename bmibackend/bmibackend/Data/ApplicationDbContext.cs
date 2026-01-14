c0ad13fb1865524bc6bb37250b2f844402bbf45e
Kavindu-Madhushanka <madu10275@gmail.com>
2026-01-14 12:22:51 +0530

    add data folder and create applicationDbContext.cs

---
+++ bmibackend/bmibackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using bmibackend.Models;
+namespace bmibackend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
