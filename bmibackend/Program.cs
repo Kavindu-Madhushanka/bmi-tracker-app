b1f551e0f724b630fba4fbaca44120462d402d81
Kavindu-Madhushanka <madu10275@gmail.com>
2026-01-15 01:36:56 +0530

    chang unnessary file path

---
+++ bmibackend/Program.cs
@@ -0,0 +1,42 @@
+﻿using bmibackend.Data;
+using bmibackend.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+// DB Config
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// CORS for React
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+// Register services
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Logging
+builder.Services.AddLogging();
+
+var app = builder.Build();
+
+// HTTP pipeline
+app.UseHttpsRedirection();
+app.UseCors("ReactApp");
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
